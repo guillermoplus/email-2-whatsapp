3186748257da944c00dcd36ec4e216dd93d2d687
guillermoplus <luisgal93@hotmail.com>
2024-10-21 21:07:39 -0500

    add microsoft graph library

--- src/jobs/admin-payment-receipt.job.ts
+++ src/jobs/admin-payment-receipt.job.ts
@@ -1,4 +1,5 @@
 import {CronJob} from "cron";
+import {OutlookService} from "../services/outlook.service";
 
 export const adminPaymentReceiptJobFactory = () => {
   // Run on every 05 of each month at 11:59:00 AM.
@@ -6,12 +7,21 @@ export const adminPaymentReceiptJobFactory = () => {
   return CronJob.from({
     // cronTime: '00 59 11 05 * *',
     cronTime: '* * * * *',
-    onTick: (onComplete) => {
+    onTick: async (onComplete) => {
       console.log('Cron job is running...');
-      // Sleep for 5 seconds
-      setTimeout(() => {
+      try {
+        const users = await OutlookService.getInstance().listUsers();
+        if(!users.length) {
+          console.warn('No users found!');
+          return;
+        }
+        const userId = users[0].id;
+        await OutlookService.getInstance().getUserEmails(userId);
+      } catch (e: any) {
+        console.error('Error:', e);
+      } finally {
         onComplete();
-      }, 5000);
+      }
     },
     onComplete: () => {
       console.log('Cron job has finished!');
--- src/services/outlook.service.ts
+++ src/services/outlook.service.ts
@@ -1,27 +1,61 @@
 import { Client } from '@microsoft/microsoft-graph-client';
-import dotenv from 'dotenv';
+import { ClientSecretCredential } from '@azure/identity';
 
-dotenv.config();
+export class OutlookService {
+  private static instance: OutlookService;
+  private grahpClient: Client;
 
-const getGraphClient = () => {
-  return Client.init({
-    authProvider: (done) => {
-      done(null, process.env.OUTLOOK_ACCESS_TOKEN); // Token de acceso de Outlook
-    },
-  });
-};
+  private CLIENT_ID = process.env.OUTLOOK_CLIENT_ID ?? '';
+  private TENANT_ID = process.env.OUTLOOK_TENANT_ID ?? '';
+  private CLIENT_SECRET = process.env.OUTLOOK_CLIENT_SECRET ?? '';
 
-export const searchEmailsByKeyword = async (keyword: string) => {
-  try {
-    const client = getGraphClient();
-    const response = await client.api('/me/messages').get();
-    const emails = response.value;
-
-    return emails.filter((email: any) =>
-      email.body.content.includes(keyword)
+  private constructor() {
+    const credential = new ClientSecretCredential(
+      this.TENANT_ID,
+      this.CLIENT_ID,
+      this.CLIENT_SECRET
     );
-  } catch (error) {
-    console.error('Error buscando correos:', error);
-    throw error;
+
+    this.grahpClient = Client.initWithMiddleware({
+      authProvider: {
+        getAccessToken: async () => {
+          const token = await credential.getToken('https://graph.microsoft.com/.default');
+          return token?.token ?? '';
+        },
+      },
+    });
   }
-};
+
+  public static getInstance(): OutlookService {
+    if (!OutlookService.instance) {
+      OutlookService.instance = new OutlookService();
+    }
+    return OutlookService.instance;
+  }
+
+  async getUserEmails(userId: string) {
+    try {
+      const emails = await this.grahpClient
+        .api(`/users/${userId}/messages`)
+        .select('subject,sender,receivedDateTime')
+        .get();
+
+      console.log('User Emails:', emails.value);
+      return emails.value;
+    } catch (error) {
+      console.error('Error retrieving emails:', error);
+      throw new Error('Failed to retrieve emails.');
+    }
+  }
+
+  async listUsers() {
+    try {
+      const users = await this.grahpClient.api('/users').get();
+      console.log('Users:', users.value);
+      return users.value;
+    } catch (error) {
+      console.error('Error retrieving users:', error);
+      throw new Error('Failed to retrieve users.');
+    }
+  }
+}
